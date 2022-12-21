d040e4651129eae303c96f42bb9fe09e83404d81
Jamong-i <smkim961211@gmail.com>
2022-12-21 15:57:20 +0900

    Feat(iOS): ToDoList funtion
    할일 목록 등록 및 취소 기능, 메모리 절약 기능

---
+++ ToDoList/Task.swift
@@ -0,0 +1,13 @@
+//
+//  Task.swift
+//  ToDoList
+//
+//  Created by Jamong on 2022/12/21.
+//
+
+import Foundation
+
+struct Task {
+    var title: String
+    var done: Bool
+}
---
+++ ToDoList/ViewController.swift
@@ -0,0 +1,53 @@
+//
+//  ViewController.swift
+//  ToDoList
+//
+//  Created by Jamong on 2022/12/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+    var tasks = [Task]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.tableView.dataSource = self
+    }
+    
+    @IBAction func tapEditButton(_ sender: UIBarButtonItem) {
+    }
+    
+    @IBAction func tapAddButton(_ sender: UIBarButtonItem) {
+        let alert = UIAlertController(title: "할 일 등록", message: "할 일을 입력해주세요.", preferredStyle: .alert)
+        let registerButton =  UIAlertAction(title: "등록", style: .default, handler: { [weak self] _ in
+            guard let title = alert.textFields?[0].text else { return }
+            let task = Task(title: title, done: false)
+            self?.tasks.append(task)
+            self?.tableView.reloadData()
+        })
+        let cancelButton = UIAlertAction(title: "취소", style: .cancel, handler: nil)
+        alert.addAction(registerButton)
+        alert.addAction(cancelButton)
+        alert.addTextField(configurationHandler: { textFiled in
+            textFiled.placeholder = "할 일을 입력해주세요."
+        })
+        self.present(alert, animated: true, completion: nil)
+    }
+    
+}
+
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.tasks.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        let task = self.tasks[indexPath.row]
+        cell.textLabel?.text = task.title
+        return cell
+    }
+}
